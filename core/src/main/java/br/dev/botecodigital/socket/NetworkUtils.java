ec61db9380a76fdd39467dcb8e7cdc3c81581ff1
Rodrigo Aramburu <rodrigoaramburu@gmail.com>
2026-01-15 08:40:08 -0300

    Consertado: listado todos os endereços de rede do host

--- core/src/main/java/br/dev/botecodigital/socket/NetworkUtils.java
+++ core/src/main/java/br/dev/botecodigital/socket/NetworkUtils.java
@@ -2,11 +2,24 @@ package br.dev.botecodigital.socket;
 
 import java.net.NetworkInterface;
 import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.badlogic.gdx.Gdx;
+
 import java.net.InetAddress;
 
 public class NetworkUtils {
 
-    public static String getLocalIPAddress() {
+
+    public static Map<String, String> getInterfacesIp() {
+        
+        Map<String, String> interfacesIp = new HashMap<>();
+        
+        String ethernetIP = null;
+        String wifiIP = null;
+        String fallbackIP = null;
+
         try {
             Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
             while (interfaces.hasMoreElements()) {
@@ -15,19 +28,29 @@ public class NetworkUtils {
                 if (iface.isLoopback() || !iface.isUp())
                     continue;
 
+                String name = iface.getName().toLowerCase();
+                Gdx.app.log("NETWORK", name);
+                // boolean isEthernet = name.startsWith("eth") || name.startsWith("en");
+                // boolean isWifi = name.startsWith("wlan") || name.startsWith("wl");
+
                 Enumeration<InetAddress> addresses = iface.getInetAddresses();
                 while (addresses.hasMoreElements()) {
                     InetAddress addr = addresses.nextElement();
 
-                    if (!addr.isLoopbackAddress() && addr.isSiteLocalAddress()) {
-                        return addr.getHostAddress();
-                    }
+                    if (addr.isLoopbackAddress() || !addr.isSiteLocalAddress())
+                        continue;
+
+                    String ip = addr.getHostAddress();
+                    interfacesIp.put(name, ip);
+                    Gdx.app.log("NETWORK", ip);
+                    Gdx.app.log("NETWORK", "------------------");
+                    
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        return "127.0.0.1";
+        Gdx.app.log("NETWORK", interfacesIp.toString());
+        return interfacesIp;
     }
 }
